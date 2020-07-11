c08445b2264e243ca69a3f86b6df375fcfef389c
chenyus0609 <48789942+chenyus0609@users.noreply.github.com>
2020-07-11 10:54:16 +0800

    Add files via upload

---
+++ python/Cutdata.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 11 10:44:51 2020
+
+@author: 陳鈺升
+"""
+
+import pandas as pd
+
+INFILE = "A1.csv"
+OUTFILE = "data"+INFILE
+
+df = pd.read_csv(INFILE)
+
